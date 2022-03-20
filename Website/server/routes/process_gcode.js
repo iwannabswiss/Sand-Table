f8030b0f7bea4405d2ae006cb80eccf846a50021
Nathan Woelfle <nwoelfle@atlas-tech.com>
2022-03-20 18:58:01 -0400

    Added support for direct gcode upload with boundary check

---
+++ Website/server/routes/process_gcode.js
@@ -0,0 +1,126 @@
+require("dotenv").config();
+
+var fs = require('fs')
+    , es = require('event-stream');
+
+const {
+    Worker, isMainThread, parentPort, workerData
+} = require('worker_threads');
+
+const fillModeOptions = {
+    FILL: 0,
+    FIT: 1
+}
+
+const X_SIZE = process.env.X_SIZE;
+const Y_SIZE = process.env.Y_SIZE;
+//const MARGIN = 5;
+var scaleGcode = parseFloat(Math.min(1000 / X_SIZE, 1000 / Y_SIZE));
+scaleGcode = scaleGcode.toFixed(2);
+
+var _;
+
+function scale([x, y], r=0, fillMode = fillModeOptions.Fit) {
+	//console.log(`Before check, X${x} Y${y}`);
+	if (fillMode == fillModeOptions.FILL) {
+		x *= parseFloat(scaleGcode);
+		y *= parseFloat(scaleGcode);
+	}
+	//check x/y to make sure within table boundaries
+	if (!(x >= 0) || !(x <= X_SIZE)) {
+		//console.log('Changing X');
+		if (x < 0) {
+			x = parseFloat(0);
+		}
+		else {
+			x = parseFloat(X_SIZE);
+		}
+	}
+	if (!(y >= 0) || !(y <= Y_SIZE)) {
+		//console.log('Changing Y');
+		if (y < 0) {
+			y = parseFloat(0);
+		}
+		else {
+			y = parseFloat(Y_SIZE);
+		}
+	}
+	//console.log(`After check, X${x} Y${y}`);
+	return [[x, y]];
+}
+
+function writeToFile(stream, fillStream, cmd, coord) {
+    var [fitCoord, _] = scale(coord, 0, fillModeOptions.FIT);
+    stream.write(`${cmd} X${fitCoord[0].toFixed(3)} Y${fitCoord[1].toFixed(3)}\n`);
+    	//doesn't check scale
+	//stream.write(`${cmd} X${coord[0].toFixed(3)} Y${coord[1].toFixed(3)}\n`);
+
+    var [fillCoord, _] = scale(coord, 0, fillModeOptions.FILL);
+    fillStream.write(`${cmd} X${fillCoord[0].toFixed(3)} Y${fillCoord[1].toFixed(3)}\n`);
+    	//doesn't check scale
+	//fillStream.write(`${cmd} X${coord[0].toFixed(3)} Y${coord[1].toFixed(3)}\n`);
+}
+
+function process_file(filename, callback) {
+
+    var prevX, prevY = null;
+
+    var outStream = fs.createWriteStream(__dirname + "/../../files/" + filename + ".gcode");
+    var outFillStream = fs.createWriteStream(__dirname + "/../../files/" + filename + " (fill).gcode");
+
+    var inStream = fs.createReadStream(__dirname + "/../../files/org/" + filename + ".gcode")
+        .pipe(es.split())
+        .pipe(es.mapSync(function (line) {
+
+            // pause the readstream
+            inStream.pause();
+
+            // process line here and call s.resume() when ready
+            // function below was for logging memory usage
+            if (line.length > 0 && !line.startsWith("#") && !line.startsWith("//") && !line.startsWith(";")) {
+                // Process line...not a comment
+                var [gValue, xCord, yCord] = line.split(new RegExp("\\s+")).filter((v) => { return v.length > 0 });
+		//console.log("Split Values: G: " + gValue + " Xcord: " + xCord + " Ycord: " + yCord)
+		xCord = parseFloat(xCord.slice(1));
+		yCord = parseFloat(yCord.slice(1));
+		writeToFile(outStream, outFillStream, "G0", [xCord, yCord]);
+            } //End of if (line.length)
+
+            // resume the readstream, possibly from a callback
+            setImmediate(() => inStream.resume());
+        })
+            .on('error', function (err) {
+                console.log('Error while reading file.', err);
+            })
+            .on('end', function () {
+                console.log('Done converting ' + filename + '.gcode to new gcode');
+                outStream.end(() => callback());
+            })
+        );
+}
+
+function process_gcode_file_to_gcode(filename, callback) {
+    // Use worker thread
+    const id = Math.random(); // Random uuid to distinguish this call from others to avoid race conditions
+    const worker = new Worker(__filename, {
+        workerData: { fn: "process_file", filename: filename, id: id }
+    });
+    worker.on('message', (val) => {
+        if (val == id)
+            callback();
+    });
+}
+
+module.exports.X_SIZE = X_SIZE;
+module.exports.Y_SIZE = Y_SIZE;
+
+if (isMainThread) {
+    module.exports.process_gcode_file_to_gcode = process_gcode_file_to_gcode;
+} else {
+    // A worker thread has been created, call the requested function
+    switch (workerData.fn) {
+        case "process_file":
+            process_file(workerData.filename, () => parentPort.postMessage(workerData.id));
+            break;
+    }
+}
--- Website/server/routes/process_upload.js
+++ Website/server/routes/process_upload.js
@@ -2,6 +2,7 @@ import express from 'express';
 import fs from 'fs';
 import fileupload from 'express-fileupload';
 import sharp from 'sharp';
+import { process_gcode_file_to_gcode } from './process_gcode';
 import { process_thr_file_to_gcode } from './process_theta_rho';
 import { process_gcode_file_to_png } from './create_png_from_gcode';
 import { renameTrack, deleteTrack } from "./playlist_manager";
@@ -20,11 +21,24 @@ router.post('/', function (req, res) {
   // The name of the input field (i.e. "file") is used to retrieve the uploaded file
   let thrFile = req.files.file;
 
-  if (!thrFile.name.endsWith(".thr")) {
+  if (!thrFile.name.endsWith(".thr") && !thrFile.name.endsWith(".gcode")) {
     return res.status(400).send("Invalid file type");
   }
-  var filename = thrFile.name.replace(".thr", "");
 
+  if (thrFile.name.endsWith(".thr")) {
+  	var filename = thrFile.name.replace(".thr", "");
+  }
+  else {
+	  var filename = thrFile.name.replace(".gcode", "");
+  }
+
+  //check if original file exists
+  if (fs.existsSync(__dirname + "/../../files/org/" + filename + ".thr"))
+    fs.unlinkSync(__dirname + "/../../files/org/" + filename + ".thr");
+  if (fs.existsSync(__dirname + "/../../files/org/" + filename + ".gcode"))
+    fs.unlinkSync(__dirname + "/../../files/org/" + filename + ".gcode");
+
+  //check if altered file exists (update to only check if org file exists)
   if (fs.existsSync(__dirname + "/../../files/" + filename + ".thr"))
     fs.unlinkSync(__dirname + "/../../files/" + filename + ".thr");
   if (fs.existsSync(__dirname + "/../../files/" + filename + ".gcode"))
@@ -39,10 +53,11 @@ router.post('/', function (req, res) {
     fs.unlinkSync(__dirname + "/../../files/" + filename + " (fill).png");
 
   // Use the mv() method to place the file somewhere on your server
-  thrFile.mv(__dirname + "/../../files/" + thrFile.name, function (err) {
+  thrFile.mv(__dirname + "/../../files/org/" + thrFile.name, function (err) {
     if (err)
       return res.status(500).send(err);
-
+ 
+  if (thrFile.name.endsWith(".thr")) {
     process_thr_file_to_gcode(filename, () => {
       process_gcode_file_to_png(filename, () => {
         sharp(__dirname + "/../../files/" + filename + ".png")
@@ -56,15 +71,42 @@ router.post('/', function (req, res) {
                   (err, info) => {
                     res.send('File uploaded!');
                   }
-                );
-            });
-      });
-    });
-  });
-});
+                ); //.toFile
+            }); //.toFile
+      }); //process_gcode_file_to_png
+    }); //process_thr_file_to_gcode
+  }
+  else {
+   process_gcode_file_to_gcode(filename, () => {
+      process_gcode_file_to_png(filename, () => {
+        sharp(__dirname + "/../../files/" + filename + ".png")
+          .extract({ left: 73, top: 73, width: 354, height: 354 }) // See https://www.desmos.com/calculator/rbn4tjbjjd for rationale of numbers
+          .resize(500) // assuming image ("/../../files/" + filename + ".png") is of width/height 500
+          .toFile(__dirname + "/../../files/" + filename + " (fill).png",
+            (err, info) => {
+              sharp(__dirname + "/../../files/" + filename + ".png")
+                .resize(100)
+                .toFile(__dirname + "/../../files/" + filename + "-small.png",
+                  (err, info) => {
+                    res.send('File uploaded!');
+                  }
+                ); //.toFile
+            }); //.toFile
+      }); //process_gcode_file_to_png
+   }); //process_gcode_file_to_gcode
+  } //EOE
+  }); //thrFile.mv
+}); //router.post
 
 router.post("/delete", function (req, res) {
   var filename = req.body.filename;
+  //check if org files exists
+  if (fs.existsSync(__dirname + "/../../files/org/" + filename + ".thr"))
+    fs.unlinkSync(__dirname + "/../../files/org/" + filename + ".thr");
+  if (fs.existsSync(__dirname + "/../../files/org/" + filename + ".gcode"))
+    fs.unlinkSync(__dirname + "/../../files/org/" + filename + ".gcode");
+
+  //check if altered files exist (update to only check if org files exists)
   if (fs.existsSync(__dirname + "/../../files/" + filename + ".thr"))
     fs.unlinkSync(__dirname + "/../../files/" + filename + ".thr");
   if (fs.existsSync(__dirname + "/../../files/" + filename + ".gcode"))
@@ -84,6 +126,15 @@ router.post("/delete", function (req, res) {
 router.post("/rename", function (req, res) {
   var filename = req.body.filename;
   var newName = req.body.newName;
+  //rename org files
+  if (fs.existsSync(__dirname + "/../../files/org/" + filename + ".thr"))
+    fs.renameSync(__dirname + "/../../files/org/" + filename + ".thr",
+      __dirname + "/../../files/org/" + newName + ".thr");
+  if (fs.existsSync(__dirname + "/../../files/org/" + filename + ".gcode"))
+    fs.renameSync(__dirname + "/../../files/org/" + filename + ".gcode",
+      __dirname + "/../../files/org/" + newName + ".gcode");
+
+  //rename altered files
   if (fs.existsSync(__dirname + "/../../files/" + filename + ".thr"))
     fs.renameSync(__dirname + "/../../files/" + filename + ".thr",
       __dirname + "/../../files/" + newName + ".thr");
